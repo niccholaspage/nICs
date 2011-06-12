25c24872b3f3cd65e44c732a3f2c511af757c802
Nicholas "niccholaspage" Nassar <niccholaspage@gmail.com>
2011-06-12 00:17:33 -0400

    Fix weather ic

--- src/main/java/com/niccholaspage/nICs/ics/N1001.java
+++ src/main/java/com/niccholaspage/nICs/ics/N1001.java
@@ -1,5 +1,6 @@
 package com.niccholaspage.nICs.ics;
 
+import org.bukkit.block.Sign;
 import org.bukkit.event.block.BlockRedstoneEvent;
 
 import com.niccholaspage.nICs.nICs;
@@ -10,7 +11,8 @@ public class N1001 implements IC {
 	public Boolean run(nICs plugin, BlockRedstoneEvent event) {
 		Boolean power = (event.getNewCurrent() > 0);
 		if (power){
-			event.getBlock().getWorld().setThundering(true);
+			Sign sign = (Sign)event.getBlock().getState();
+			event.getBlock().getWorld().setStorm(sign.getLine(2) == "on" ? true : false);
 		}
 		return null;
 	}
@@ -22,7 +24,11 @@ public class N1001 implements IC {
 
 	@Override
 	public String canPlace(String[] lines) {
-		return null;
+		if (!(lines[2].equalsIgnoreCase("on") || lines[2].equalsIgnoreCase("off"))){
+			return null;
+		}else {
+			return "Line 3 must be 'on' or 'off.'";
+		}
 	}
 
 }
